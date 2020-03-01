68320489776d1749b77c1b4cfe3287db933252d0
Louis Chuang <louis.chuang@thinkpower-info.com>
2020-03-01 17:58:00 +0800

    finish mixins

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Mixins - List1</h1>
+        <p>{{text}}</p>
+        <input v-model="filterStore" />
+        <ul>
+          <li v-for="store in filteredStores" :key="store">{{ store }}</li>
+        </ul>
+      </div>
+      <hr />
+      <store-list></store-list>
+    </div>
+  </div>
 </template>
 
 <script>
+import StoreList from "./StoreList.vue";
 
+export default {
+  data() {
+    return {
+      text: "Vue Lecture-2",
+      stores: ["KENUKE", "Eslite", "Uniqlo"],
+      filterStore: ""
+    };
+  },
+  computed: {
+    filteredStores() {
+      return this.stores.filter(element => {
+        return element.toLowerCase().match(this.filterStore.toLowerCase());
+      });
+    }
+  },
+  components: {
+    StoreList
+  },
+  created() {
+    console.log('')
+  }
+};
 </script>
 
-<style lang="scss">
-
+<style>
 </style>
---
+++ src/StoreList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Mixins - List2</h1>
+                <input v-model="filterStore">
+                <ul>
+                    <li v-for="store in filteredStores" :key="store">{{ store }}</li>
+                </ul>
+            </div>
+            <button @click="stores.push('Test Item')">Add Test Item</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import mixins
+
+    export default {
+        // use mixins array
+        data() {
+            return {
+                stores: ['KENUKE', 'Eslite', 'Uniqlo'],
+                filterStore: ''
+            }
+        },
+        computed: {
+            filteredStores() {
+                return this.stores.filter((element) => {
+                    return element.toLowerCase().match(this.filterStore.toLowerCase());
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
