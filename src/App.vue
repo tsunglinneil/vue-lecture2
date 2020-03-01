99271f6bdad221ad8385f4dffc11f67d0ad54afe
Louis Chuang <louis.chuang@thinkpower-info.com>
2020-03-01 12:35:42 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
-    <form v-if="!isSubmitted">
+    <form>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>員工檔案建立</h1>
           <hr />
+          <!-- v-model - TEXT -->
           <div class="form-group">
             <label for="nametc">姓名</label>
             <input type="text" id="nametc" class="form-control" v-model="employee.nametc" />
           </div>
+          <!-- v-model - behind the scense -->
           <div class="form-group">
             <label for="mail">信箱</label>
             <input
@@ -25,6 +27,7 @@
           </div>
         </div>
       </div>
+      <!-- v-model - TEXT AREA -->
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="intro">自我介紹</label>
@@ -32,6 +35,7 @@
           <textarea id="intro" rows="5" class="form-control" v-model="employee.intro"></textarea>
         </div>
       </div>
+      <!-- v-model - CHECKBOX -->
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
@@ -45,6 +49,7 @@
           </div>
         </div>
       </div>
+      <!-- v-model - RADIO BUTTON -->
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="male">
@@ -55,29 +60,30 @@
           </label>
         </div>
       </div>
+      <!-- v-model - DROPDOWN -->
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="department">事業處</label>
-          <!-- <select id="department" class="form-control">
+          <select id="department" class="form-control">
             <option></option>
-          </select>-->
-          <select-department :options="department" v-model="departmentIdx"></select-department>
+          </select>
+          <!-- <select-department :options="department" v-model="departmentIdx"></select-department> -->
         </div>
 
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="division">部門別</label>
-          <!-- <select id="division" class="form-control">
+          <select id="division" class="form-control">
             <option></option>
-          </select>-->
-          <select-division :options="division" v-model="divisionIdx"></select-division>
+          </select>
+          <!-- <select-division :options="division" v-model="divisionIdx"></select-division> -->
         </div>
 
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="group">組別</label>
-          <!-- <select id="group" class="form-control">
+          <select id="group" class="form-control">
             <option></option>
-          </select>-->
-          <select-group :options="group" v-model="groupIdx"></select-group>
+          </select>
+          <!-- <select-group :options="group" v-model="groupIdx"></select-group> -->
         </div>
       </div>
       <hr />
@@ -88,7 +94,7 @@
       </div>
     </form>
     <hr />
-    <div class="row" v-if="isSubmitted">
+    <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
