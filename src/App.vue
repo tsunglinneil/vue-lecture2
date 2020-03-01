8acacbad859979dbb408af5d42ca05da2c7d3239
Louis Chuang <louis.chuang@thinkpower-info.com>
2020-03-01 19:03:57 +0800

    finish router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Routing Demo</h1>
+        <hr />
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style lang="scss">
-
 </style>
