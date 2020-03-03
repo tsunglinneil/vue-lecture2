c968ed46ac2eede94bfb99259029378ec0a1946c
tsunglinneil <magic3657@gmail.com>
2020-03-03 10:59:32 +0800

    finish demo6

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Get Current Counter By Store: {{ currentCounterByStore }}</p>
+    <p>
+      Get Current Counter By Store Getters: {{ currentCounterByStoreGetters }}
+    </p>
+    <p>Get Current Counter By mapGetters: {{ currentCounter }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    // 從vue instance 中 $store物件取得central store的state
+    currentCounterByStore() {
+      return this.$store.state.counter;
+    },
+    // 從vue instance 中 $store物件取得central store的getters
+    currentCounterByStoreGetters() {
+      return this.$store.getters.currentCounter;
+    },
+    // 從vuex提供mapGetters helper function 協助更簡潔的取得central store的getters
+    ...mapGetters(["currentCounter"]),
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    counter: 0
+  },
+  getters: {
+    currentCounter: function(state) {
+      console.log(state);
+      return state.counter;
+    }
+  }
+})
+
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
